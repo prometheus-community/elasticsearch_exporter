737dd57f172d3dbbc3cbf71bb580f587262afc0d
Manabu Matsuzaki <matsumana@gmail.com>
2017-08-17 00:21:01 +0900

    add index metrics via alias

---
+++ collector/aliases.go
@@ -0,0 +1,166 @@
+package collector
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	defaultAliasLabels      = []string{"cluster", "alias", "index"}
+	defaultAliasLabelValues = func(clusterName string, alias string, indexName string) []string {
+		return []string{clusterName, alias, indexName}
+	}
+)
+
+type aliasMetric struct {
+	Type   prometheus.ValueType
+	Desc   *prometheus.Desc
+	Value  func(indexStats IndexStatsIndexResponse) float64
+	Labels func(clusterName string, alias string, indexName string) []string
+}
+
+type Aliases struct {
+	logger log.Logger
+	client *http.Client
+	url    *url.URL
+
+	up                prometheus.Gauge
+	totalScrapes      prometheus.Counter
+	jsonParseFailures prometheus.Counter
+
+	aliasMetrics []*aliasMetric
+}
+
+func NewAliases(logger log.Logger, client *http.Client, url *url.URL) *Aliases {
+	return &Aliases{
+		logger: logger,
+		client: client,
+		url:    url,
+
+		up: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: prometheus.BuildFQName(namespace, "alias_stats", "up"),
+			Help: "Was the last scrape of the ElasticSearch alias endpoint successful.",
+		}),
+		totalScrapes: prometheus.NewCounter(prometheus.CounterOpts{
+			Name: prometheus.BuildFQName(namespace, "alias_stats", "total_scrapes"),
+			Help: "Current total ElasticSearch alias scrapes.",
+		}),
+		jsonParseFailures: prometheus.NewCounter(prometheus.CounterOpts{
+			Name: prometheus.BuildFQName(namespace, "alias_stats", "json_parse_failures"),
+			Help: "Number of errors while parsing JSON.",
+		}),
+
+		aliasMetrics: []*aliasMetric{
+			{
+				Type: prometheus.GaugeValue,
+				Desc: prometheus.NewDesc(
+					prometheus.BuildFQName(namespace, "aliases", "docs_primary"),
+					"Count of documents which only primary shards via alias",
+					defaultAliasLabels, nil,
+				),
+				Value: func(indexStats IndexStatsIndexResponse) float64 {
+					return float64(indexStats.Primaries.Docs.Count)
+				},
+				Labels: defaultAliasLabelValues,
+			},
+		},
+	}
+}
+
+func (i *Aliases) Describe(ch chan<- *prometheus.Desc) {
+	for _, metric := range i.aliasMetrics {
+		ch <- metric.Desc
+	}
+	ch <- i.up.Desc()
+	ch <- i.totalScrapes.Desc()
+	ch <- i.jsonParseFailures.Desc()
+}
+
+func (c *Aliases) fetchAndDecodeStats() (aliasesResponse, error) {
+	var ar aliasesResponse
+
+	u := *c.url
+	u.Path = "/_aliases"
+
+	res, err := c.client.Get(u.String())
+	if err != nil {
+		return ar, fmt.Errorf("failed to get alias from %s://%s:%s/%s: %s",
+			u.Scheme, u.Hostname(), u.Port(), u.Path, err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		return ar, fmt.Errorf("HTTP Request failed with code %d", res.StatusCode)
+	}
+
+	if err := json.NewDecoder(res.Body).Decode(&ar); err != nil {
+		c.jsonParseFailures.Inc()
+		return ar, err
+	}
+	return ar, nil
+}
+
+func (a *Aliases) Collect(ch chan<- prometheus.Metric) {
+	a.totalScrapes.Inc()
+	defer func() {
+		ch <- a.up
+		ch <- a.totalScrapes
+		ch <- a.jsonParseFailures
+	}()
+
+	// clusterHealth
+	clusterHealth := NewClusterHealth(a.logger, a.client, a.url)
+	clusterHealthResponse, err := clusterHealth.fetchAndDecodeClusterHealth()
+	if err != nil {
+		a.up.Set(0)
+		level.Warn(a.logger).Log(
+			"msg", "failed to fetch and decode cluster health",
+			"err", err,
+		)
+		return
+	}
+
+	// indices
+	indices := NewIndices(a.logger, a.client, a.url)
+	indexStatsResponse, err := indices.fetchAndDecodeStats()
+	if err != nil {
+		a.up.Set(0)
+		level.Warn(a.logger).Log(
+			"msg", "failed to fetch and decode index stats",
+			"err", err,
+		)
+		return
+	}
+
+	// aliases
+	aliasesResponse, err := a.fetchAndDecodeStats()
+	if err != nil {
+		a.up.Set(0)
+		level.Warn(a.logger).Log(
+			"msg", "failed to fetch and decode alias",
+			"err", err,
+		)
+		return
+	}
+
+	a.up.Set(1)
+
+	for indexName, aliases := range aliasesResponse {
+		for alias := range aliases["aliases"] {
+			for _, metric := range a.aliasMetrics {
+				indexStatsIndexResponse := indexStatsResponse.Indices[indexName]
+				ch <- prometheus.MustNewConstMetric(
+					metric.Desc,
+					metric.Type,
+					metric.Value(indexStatsIndexResponse),
+					metric.Labels(clusterHealthResponse.ClusterName, alias, indexName)...,
+				)
+			}
+		}
+	}
+}
---
+++ collector/aliases_test.go
@@ -0,0 +1,48 @@
+package collector
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/go-kit/kit/log"
+)
+
+func TestAliases(t *testing.T) {
+	// Testcases created using:
+	//  docker run -d -p 9200:9200 elasticsearch:VERSION-alpine
+	//  curl -XPUT http://localhost:9200/foo_1
+	//  curl -XPUT http://localhost:9200/foo_2
+	//  curl -XPOST {"actions":[{"add":{"index":"foo_1","alias":"foo"}},{"add":{"index":"foo_1","alias":"fooA"}},{"add":{"index":"foo_2","alias":"fooB"}}]}
+	ta := map[string]string{
+		"5.5.1": `{"foo_1":{"aliases":{"foo":{},"fooA":{}}},"foo_2":{"aliases":{"fooB":{}}}}`,
+	}
+	for ver, out := range ta {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintln(w, out)
+		}))
+		defer ts.Close()
+
+		u, err := url.Parse(ts.URL)
+		if err != nil {
+			t.Fatalf("Failed to parse URL: %s", err)
+		}
+		a := NewAliases(log.NewNopLogger(), http.DefaultClient, u)
+		stats, err := a.fetchAndDecodeStats()
+		if err != nil {
+			t.Fatalf("Failed to fetch or decode indices stats: %s", err)
+		}
+		t.Logf("[%s] Index Response: %+v", ver, stats)
+		if len(stats) != 2 {
+			t.Errorf("Wrong number of indices")
+		}
+		if len(stats["foo_1"]["aliases"]) != 2 {
+			t.Errorf("Wrong number of aliases")
+		}
+		if len(stats["foo_2"]["aliases"]) != 1 {
+			t.Errorf("Wrong number of aliases")
+		}
+	}
+}
