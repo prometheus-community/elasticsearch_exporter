541a5508fc52243faa2817d150a69a315432bcfb
Manabu Matsuzaki <matsumana@gmail.com>
2017-08-29 20:00:14 +0900

    Add tests for each supported version

--- collector/aliases_test.go
+++ collector/aliases_test.go
@@ -15,9 +15,13 @@ func TestAliases(t *testing.T) {
 	//  docker run -d -p 9200:9200 elasticsearch:VERSION-alpine
 	//  curl -XPUT http://localhost:9200/foo_1
 	//  curl -XPUT http://localhost:9200/foo_2
-	//  curl -XPOST {"actions":[{"add":{"index":"foo_1","alias":"foo"}},{"add":{"index":"foo_1","alias":"fooA"}},{"add":{"index":"foo_2","alias":"fooB"}}]}
+	//  curl -XPOST http://localhost:9200/_aliases -d '{"actions":[{"add":{"index":"foo_1","alias":"foo"}},{"add":{"index":"foo_1","alias":"fooA"}},{"add":{"index":"foo_2","alias":"fooB"}}]}'
+	//  curl http://localhost:9200/_aliases
 	ta := map[string]string{
-		"5.5.1": `{"foo_1":{"aliases":{"foo":{},"fooA":{}}},"foo_2":{"aliases":{"fooB":{}}}}`,
+		"1.7.6": `{"foo_1":{"aliases":{"foo":{},"fooA":{}}},"foo_2":{"aliases":{"fooB":{}}}}`,
+		"2.4.5": `{"foo_2":{"aliases":{"fooB":{}}},"foo_1":{"aliases":{"foo":{},"fooA":{}}}}`,
+		"5.4.2": `{".monitoring-alerts-2":{"aliases":{}},".monitoring-data-2":{"aliases":{}},".watches":{"aliases":{}},".monitoring-es-2-2017.08.29":{"aliases":{}},".triggered_watches":{"aliases":{}},"foo_1":{"aliases":{"foo":{},"fooA":{}}},".watcher-history-3-2017.08.29":{"aliases":{}},"foo_2":{"aliases":{"fooB":{}}}}`,
+		"5.5.2": `{".watches":{"aliases":{}},".monitoring-alerts-6":{"aliases":{}},"foo_2":{"aliases":{"fooB":{}}},".monitoring-es-6-2017.08.29":{"aliases":{}},".watcher-history-3-2017.08.29":{"aliases":{}},".triggered_watches":{"aliases":{}},"foo_1":{"aliases":{"foo":{},"fooA":{}}}}`,
 	}
 	for ver, out := range ta {
 		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -35,9 +39,6 @@ func TestAliases(t *testing.T) {
 			t.Fatalf("Failed to fetch or decode indices stats: %s", err)
 		}
 		t.Logf("[%s] Index Response: %+v", ver, stats)
-		if len(stats) != 2 {
-			t.Errorf("Wrong number of indices")
-		}
 		if len(stats["foo_1"]["aliases"]) != 2 {
 			t.Errorf("Wrong number of aliases")
 		}
