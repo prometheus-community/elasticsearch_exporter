fa640451317b4dabf5fc5f625039de8e4fd2595a
Christoph Oelmüller <christoph.oelmueller@justwatch.com>
2019-02-26 10:16:08 +0100

    enable configuration of via environment variables

--- main.go
+++ main.go
@@ -17,58 +17,58 @@ func main() {
 		Name          = "elasticsearch_exporter"
 		listenAddress = kingpin.Flag("web.listen-address",
 			"Address to listen on for web interface and telemetry.").
-			Default(":9114").String()
+			Default(":9114").Envar("WEB_LISTEN_ADDRESS").String()
 		metricsPath = kingpin.Flag("web.telemetry-path",
 			"Path under which to expose metrics.").
-			Default("/metrics").String()
+			Default("/metrics").Envar("WEB_TELEMETRY_PATH").String()
 		esURI = kingpin.Flag("es.uri",
 			"HTTP API address of an Elasticsearch node.").
-			Default("http://localhost:9200").String()
+			Default("http://localhost:9200").Envar("ES_URI").String()
 		esTimeout = kingpin.Flag("es.timeout",
 			"Timeout for trying to get stats from Elasticsearch.").
-			Default("5s").Duration()
+			Default("5s").Envar("ES_TIMEOUT").Duration()
 		esAllNodes = kingpin.Flag("es.all",
 			"Export stats for all nodes in the cluster. If used, this flag will override the flag es.node.").
-			Default("false").Bool()
+			Default("false").Envar("ES_ALL").Bool()
 		esNode = kingpin.Flag("es.node",
 			"Node's name of which metrics should be exposed.").
-			Default("_local").String()
+			Default("_local").Envar("ES_NODE").String()
 		esExportIndices = kingpin.Flag("es.indices",
 			"Export stats for indices in the cluster.").
-			Default("false").Bool()
+			Default("false").Envar("ES_INDICES").Bool()
 		esExportIndicesSettings = kingpin.Flag("es.indices_settings",
 			"Export stats for settings of all indices of the cluster.").
-			Default("false").Bool()
+			Default("false").Envar("ES_INDICES_SETTINGS").Bool()
 		esExportClusterSettings = kingpin.Flag("es.cluster_settings",
 			"Export stats for cluster settings.").
-			Default("false").Bool()
+			Default("false").Envar("ES_CLUSTER_SETTINGS").Bool()
 		esExportShards = kingpin.Flag("es.shards",
 			"Export stats for shards in the cluster (implies --es.indices).").
-			Default("false").Bool()
+			Default("false").Envar("ES_SHARDS").Bool()
 		esExportSnapshots = kingpin.Flag("es.snapshots",
 			"Export stats for the cluster snapshots.").
-			Default("false").Bool()
+			Default("false").Envar("ES_SNAPSHOTS").Bool()
 		esCA = kingpin.Flag("es.ca",
 			"Path to PEM file that contains trusted Certificate Authorities for the Elasticsearch connection.").
-			Default("").String()
+			Default("").Envar("ES_CA").String()
 		esClientPrivateKey = kingpin.Flag("es.client-private-key",
 			"Path to PEM file that contains the private key for client auth when connecting to Elasticsearch.").
-			Default("").String()
+			Default("").Envar("ES_CLIENT_PRIVATE_KEY").String()
 		esClientCert = kingpin.Flag("es.client-cert",
 			"Path to PEM file that contains the corresponding cert for the private key to connect to Elasticsearch.").
-			Default("").String()
+			Default("").Envar("ES_CLIENT_CERT").String()
 		esInsecureSkipVerify = kingpin.Flag("es.ssl-skip-verify",
 			"Skip SSL verification when connecting to Elasticsearch.").
-			Default("false").Bool()
+			Default("false").Envar("ES_SSL_SKIP_VERIFY").Bool()
 		logLevel = kingpin.Flag("log.level",
 			"Sets the loglevel. Valid levels are debug, info, warn, error").
-			Default("infor").String()
+			Default("info").Envar("LOG_LEVEL").String()
 		logFormat = kingpin.Flag("log.format",
 			"Sets the log format. Valid formats are json and logfmt").
-			Default("logfmt").String()
+			Default("logfmt").Envar("LOG_FMT").String()
 		logOutput = kingpin.Flag("log.output",
 			"Sets the log output. Valid outputs are stdout and stderr").
-			Default("stdout").String()
+			Default("stdout").Envar("LOG_OUTPUT").String()
 	)
 
 	kingpin.Version(version.Print(Name))
@@ -77,10 +77,6 @@ func main() {
 
 	logger := getLogger(*logLevel, *logOutput, *logFormat)
 
-	esURIEnv, ok := os.LookupEnv("ES_URI")
-	if ok {
-		*esURI = esURIEnv
-	}
 	esURL, err := url.Parse(*esURI)
 	if err != nil {
 		_ = level.Error(logger).Log(
@@ -89,7 +85,6 @@ func main() {
 		)
 		os.Exit(1)
 	}
-
 	// returns nil if not provided and falls back to simple TCP.
 	tlsConfig := createTLSConfig(*esCA, *esClientCert, *esClientPrivateKey, *esInsecureSkipVerify)
 
@@ -120,7 +115,7 @@ func main() {
 	}
 	http.Handle(*metricsPath, prometheus.Handler())
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, err = w.Write([]byte(`<html>
+		_, err := w.Write([]byte(`<html>
 			<head><title>Elasticsearch Exporter</title></head>
 			<body>
 			<h1>Elasticsearch Exporter</h1>
