03afa232ec018c287be1820c1800ad50daa28c85
Bobby Brockway <bobby.brockway@gmail.com>
2020-04-23 12:10:24 +0100

    updated tests

--- collector/snapshots_test.go
+++ collector/snapshots_test.go
@@ -14,20 +14,25 @@ func TestSnapshots(t *testing.T) {
 	// Testcases created using $REPO/hack/snapshots_test_setup.sh
 
 	tcs := map[string][]string{
-		//"1.7.6":           {`{"test1":{"type":"fs","settings":{"location":"/tmp/test1"}}}`, `{"snapshots":[{"snapshot":"snapshot_1","version_id":1070699,"version":"1.7.6","indices":["foo_1","foo_2"],"state":"SUCCESS","start_time":"2018-09-04T09:09:02.427Z","start_time_in_millis":1536052142427,"end_time":"2018-09-04T09:09:02.755Z","end_time_in_millis":1536052142755,"duration_in_millis":328,"failures":[],"shards":{"total":10,"failed":0,"successful":10}}]}`},
-		//"2.4.5":           {`{"test1":{"type":"fs","settings":{"location":"/tmp/test1"}}}`, `{"snapshots":[{"snapshot":"snapshot_1","version_id":2040599,"version":"2.4.5","indices":["foo_2","foo_1"],"state":"SUCCESS","start_time":"2018-09-04T09:25:25.818Z","start_time_in_millis":1536053125818,"end_time":"2018-09-04T09:25:26.326Z","end_time_in_millis":1536053126326,"duration_in_millis":508,"failures":[],"shards":{"total":10,"failed":0,"successful":10}}]}`},
-		//"5.4.2":           {`{"test1":{"type":"fs","settings":{"location":"/tmp/test1"}}}`, `{"snapshots":[{"snapshot":"snapshot_1","uuid":"VZ_c_kKISAW8rpcqiwSg0w","version_id":5040299,"version":"5.4.2","indices":["foo_2","foo_1"],"state":"SUCCESS","start_time":"2018-09-04T09:29:13.971Z","start_time_in_millis":1536053353971,"end_time":"2018-09-04T09:29:14.477Z","end_time_in_millis":1536053354477,"duration_in_millis":506,"failures":[],"shards":{"total":10,"failed":0,"successful":10}}]}`},
-		//"5.4.2-failed":    {`{"test1":{"type":"fs","settings":{"location":"/tmp/test1"}}}`, `{"snapshots":[{"snapshot":"snapshot_1","uuid":"VZ_c_kKISAW8rpcqiwSg0w","version_id":5040299,"version":"5.4.2","indices":["foo_2","foo_1"],"state":"SUCCESS","start_time":"2018-09-04T09:29:13.971Z","start_time_in_millis":1536053353971,"end_time":"2018-09-04T09:29:14.477Z","end_time_in_millis":1536053354477,"duration_in_millis":506,"failures":[{"index" : "index_name","index_uuid" : "index_name","shard_id" : 52,"reason" : "IndexShardSnapshotFailedException[error deleting index file [pending-index-5] during cleanup]; nested: NoSuchFileException[Blob [pending-index-5] does not exist]; ","node_id" : "pPm9jafyTjyMk0T5A101xA","status" : "INTERNAL_SERVER_ERROR"}],"shards":{"total":10,"failed":1,"successful":10}}]}`},
-		"7.5.2":		   {`{"succeed":{"type":"fs","settings":{"location":"/tmp/succeed"}}}`, `{"snapshots":[{"snapshot":"visible","uuid":"VsEA3Y9GStOVKPy0-nAA2A","version_id":7050299,"version":"7.5.2","indices":["foo_1","foo_2"],"include_global_state":true,"state":"SUCCESS","start_time":"2020-04-22T16:18:22.700Z","start_time_in_millis":1587572302700,"end_time":"2020-04-22T16:18:23.100Z","end_time_in_millis":1587572303100,"duration_in_millis":400,"failures":[],"shards":{"total":2,"failed":0,"successful":2}}]}`},
-		"7.5.2-repo-fail": {`{"fail":{"type":"fs","settings":{"location":"/tmp/fail"}}}`, `{"error":{"root_cause":[{"type":"repository_exception","reason":"[fail] Could not determine repository generation from root blobs"}],"type":"repository_exception","reason":"[fail] Could not determine repository generation from root blobs","caused_by":{"type":"access_denied_exception","reason":"/tmp/repos/fail"}},"status":500}`},
+		"7.5.2": {
+			`{"succeed":{"type":"fs","settings":{"location":"/tmp/succeed"}},"fail":{"type":"fs","settings":{"location":"/tmp/fail"}}}`,
+			`{"snapshots":[{"snapshot":"visible","uuid":"3IKCfpc1THyLr2AuMy1eng","version_id":7050299,"version":"7.5.2","indices":["foo_1","foo_2"],"include_global_state":true,"state":"SUCCESS","start_time":"2020-04-23T10:20:25.125Z","start_time_in_millis":1587637225125,"end_time":"2020-04-23T10:20:25.525Z","end_time_in_millis":1587637225525,"duration_in_millis":400,"failures":[],"shards":{"total":2,"failed":0,"successful":2}}]}`,
+			`{"error":{"root_cause":[{"type":"repository_exception","reason":"[fail] Could not determine repository generation from root blobs"}],"type":"repository_exception","reason":"[fail] Could not determine repository generation from root blobs","caused_by":{"type":"access_denied_exception","reason":"/tmp/fail"}},"status":500}`,
+		},
 	}
 	for ver, out := range tcs {
 		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if r.RequestURI == "/_snapshot" {
+			switch r.RequestURI {
+			case "/_snapshot":
 				fmt.Fprint(w, out[0])
 				return
+			case "/_snapshot/succeed/_all":
+				fmt.Fprint(w, out[1])
+				return
+			case "/_snapshot/fail/_all":
+				fmt.Fprint(w, out[2])
+				return
 			}
-			fmt.Fprint(w, out[1])
 		}))
 		defer ts.Close()
 
@@ -41,24 +46,30 @@ func TestSnapshots(t *testing.T) {
 			t.Fatalf("Failed to fetch or decode snapshots stats: %s", err)
 		}
 		t.Logf("[%s] Snapshots Response: %+v", ver, stats)
-		succeedRepoStats := stats["succeed"]
-		//failRepoStats := stats["fail"]
-		succeedSnapshotStats := succeedRepoStats.Snapshots[0]
 
-		if len(succeedSnapshotStats.Indices) != 2 {
-			t.Errorf("Bad number of snapshot indices")
-		}
-		if len(succeedSnapshotStats.Failures) != int(succeedSnapshotStats.Shards.Failed) {
-			t.Errorf("Bad number of snapshot failures")
-		}
-		if succeedSnapshotStats.Shards.Total != 10 {
-			t.Errorf("Bad number of snapshot shards total")
-		}
-		if succeedSnapshotStats.Shards.Successful != 10 {
-			t.Errorf("Bad number of snapshot shards successful")
-		}
-		if len(succeedRepoStats.Snapshots) != 1 {
-			t.Errorf("Bad number of repository snapshots")
+		for repo, snapshotResponse := range stats {
+			if repo == "fail" {
+				if snapshotResponse.Snapshots != nil{
+					t.Errorf("Returning non-nil Snapshots response for inaccessible repo")
+				}
+				continue
+			} else {
+				if len(snapshotResponse.Snapshots[0].Indices) != 2 {
+					t.Errorf("Bad number of snapshot indices")
+				}
+				if len(snapshotResponse.Snapshots[0].Failures) != int(snapshotResponse.Snapshots[0].Shards.Failed) {
+					t.Errorf("Bad number of snapshot failures")
+				}
+				if snapshotResponse.Snapshots[0].Shards.Total != 2 {
+					t.Errorf("Bad number of snapshot shards total")
+				}
+				if snapshotResponse.Snapshots[0].Shards.Successful != 2 {
+					t.Errorf("Bad number of snapshot shards successful")
+				}
+				if len(snapshotResponse.Snapshots) != 1 {
+					t.Errorf("Bad number of repository snapshots")
+				}
+			}
 		}
 	}
 
--- hack/snapshots_test_setup.sh
+++ hack/snapshots_test_setup.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
-set -ex
+containerID=$(docker run -d --rm -p 9200:9200 -e discovery.type=single-node -e path.repo=/tmp docker.elastic.co/elasticsearch/elasticsearch:7.5.2)
 
-containerID=$(docker run -d --rm -p 9200:9200 -e "discovery.type=single-node" -e "path.repo=/tmp" docker.elastic.co/elasticsearch/elasticsearch:7.5.2)
-
-while ! curl http://localhost:9200/_cluster/health; do
-  sleep 2
+# Wait for ES to become available
+while ! curl http://localhost:9200/_cluster/health >/dev/null 2>&1; do
+    sleep 2
 done
 
+# Create indices & events
 curl -H 'Content-Type: application/json' -XPUT "http://localhost:9200/foo_1/_doc/1" -d '{"title":"abc","content":"hello"}'
 curl -H 'Content-Type: application/json' -XPUT "http://localhost:9200/foo_1/_doc/2" -d '{"title":"def","content":"world"}'
 curl -H 'Content-Type: application/json' -XPUT "http://localhost:9200/foo_2/_doc/1" -d '{"title":"abc001","content":"hello001"}'
 curl -H 'Content-Type: application/json' -XPUT "http://localhost:9200/foo_2/_doc/2" -d '{"title":"def002","content":"world002"}'
 curl -H 'Content-Type: application/json' -XPUT "http://localhost:9200/foo_2/_doc/3" -d '{"title":"def003","content":"world003"}'
 
+# Create snapshot repositories
 curl -H 'Content-Type: application/json' -XPUT "http://localhost:9200/_snapshot/succeed" -d '{"type": "fs","settings":{"location": "/tmp/succeed"}}'
 curl -H 'Content-Type: application/json' -XPUT "http://localhost:9200/_snapshot/fail" -d '{"type": "fs","settings":{"location": "/tmp/fail"}}'
 
+# Create snapshots
 curl -XPUT "http://localhost:9200/_snapshot/succeed/visible?wait_for_completion=true"
 curl -XPUT "http://localhost:9200/_snapshot/fail/notvisible?wait_for_completion=true"
 
+# Remove access to the "fail" repo
 docker exec -it "$containerID" chmod 000 /tmp/fail
+
+## Echo out test cases
+## shellcheck disable=SC2016
+#echo '{`'"$(curl http://localhost:9200/_snapshot/succeed)"'`, `'"$(curl http://localhost:9200/_snapshot/succeed/_all)"'`},'
+## shellcheck disable=SC2016
+#echo '{`'"$(curl http://localhost:9200/_snapshot/fail)"'`, `'"$(curl http://localhost:9200/_snapshot/fail/_all)"'`},'
+#
+## Kill docker container
+#docker kill "$containerID"
