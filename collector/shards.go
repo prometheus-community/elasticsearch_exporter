5dabdb88185aafbd4cc80f9d892c5ba07fb8f8b9
avinash kumar <avee137@gmail.com>
2022-02-22 14:06:12 -0500

    publish total shards on a node, fixes go-kit import
    Signed-off-by: avinash kumar <avee137@gmail.com>

--- collector/shards.go
+++ collector/shards.go
@@ -19,8 +19,8 @@ import (
 	"net/url"
 	"path"
 
-	"github.com/go-kit/kit/log"
-	"github.com/go-kit/kit/log/level"
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
