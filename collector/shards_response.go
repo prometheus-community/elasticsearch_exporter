2d11c8cd3b63ee7c660c03a981a8faa9857d468c
avinash kumar <avee137@gmail.com>
2022-02-22 10:22:35 -0500

    publish total shards on a node
    Signed-off-by: avinash kumar <avee137@gmail.com>

---
+++ collector/shards_response.go
@@ -0,0 +1,20 @@
+// Copyright 2021 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package collector
+
+// ShardResponse is a representation of an Elasticsearch Shard
+type ShardResponse struct {
+	Index string `json:"index"`
+	Shard string `json:"shard"`
+	Node  string `json:"node"`
+}
