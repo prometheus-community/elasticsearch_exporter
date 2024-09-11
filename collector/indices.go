b316587a9beb7a84df4846a8dbec6998451ba947
Tamara Bernshtein <tamara.bernshtein@riskified.com>
2024-09-11 13:37:08 +0300

    added refresh status

--- collector/indices.go
+++ collector/indices.go
@@ -811,7 +811,7 @@ func NewIndices(logger log.Logger, client *http.Client, url *url.URL, shards boo
 					indexLabels.keys(), nil,
 				),
 				Value: func(indexStats IndexStatsIndexResponse) float64 {
-					return float64(indexStats.Total.Refresh.ExternalTotal) / 1000
+					return float64(indexStats.Total.Refresh.ExternalTotalTimeInMillis) / 1000
 				},
 				Labels: indexLabels,
 			},
