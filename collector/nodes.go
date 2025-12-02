2511b22c5e78a4dd7c6cab585753c3e5f96f7574
Signed-off-by: etgraylog <evan.tepsic@graylog.com>
2025-12-02 01:45:40 -0500

    Add search role to func getRoles
    Signed-off-by: etgraylog <evan.tepsic@graylog.com>

--- collector/nodes.go
+++ collector/nodes.go
@@ -37,6 +37,7 @@ func getRoles(node NodeStatsNodeResponse) map[string]bool {
 		"data_content":          false,
 		"ml":                    false,
 		"remote_cluster_client": false,
+		"search":                false,
 		"transform":             false,
 		"ingest":                false,
 		"client":                true,
