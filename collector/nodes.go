3467e1929e85068541204b8ff122c04d293e3d16
etgraylog <evan.tepsic@graylog.com>
2025-12-02 01:49:50 -0500

    Add fileCacheMetric to Nodes struct
    Signed-off-by: etgraylog <evan.tepsic@graylog.com>

--- collector/nodes.go
+++ collector/nodes.go
@@ -187,6 +187,7 @@ type Nodes struct {
 	threadPoolMetrics         []*threadPoolMetric
 	filesystemDataMetrics     []*filesystemDataMetric
 	filesystemIODeviceMetrics []*filesystemIODeviceMetric
+	fileCacheMetrics          []*fileCacheMetric
 }
 
 // NewNodes defines Nodes Prometheus metrics
