3a78e8b6c1292775fe3a41d54f91514784066b26
dfradehubs <dfradejas@freepik.com>
2026-03-02 13:27:51 +0100

    feat: add indexing/search/delete metrics to data-stream collector
    Enrich the data-stream collector with 10 new metrics aggregated from
    backing indices, making --collector.data-stream self-sufficient without
    needing --es.indices (which causes high cardinality per backing index).
    
    New metrics (elasticsearch_data_stream_stats_*):
    - indexing_index_total, indexing_index_time_seconds_total, indexing_index_current
    - indexing_delete_total, indexing_delete_time_seconds_total
    - search_query_total, search_query_time_seconds_total
    - search_fetch_total, search_fetch_time_seconds_total
    - docs_total
    
    Also adds GHCR build workflow and multi-stage Dockerfile.
    Removes debug fmt.Printf statement.

--- collector/data_stream.go
+++ collector/data_stream.go
@@ -16,10 +16,10 @@ package collector
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"log/slog"
 	"net/http"
 	"net/url"
+	"strings"
 
 	"github.com/prometheus/client_golang/prometheus"
 )
@@ -34,6 +34,67 @@ var (
 	dataStreamStoreSizeBytes = prometheus.NewDesc(
 		prometheus.BuildFQName(namespace, "data_stream", "store_size_bytes"),
 		"Store size of data stream",
+		[]string{"data_stream"},
+		nil,
+	)
+
+	dataStreamStatsIndexingIndexTotal = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "data_stream_stats", "indexing_index_total"),
+		"Total number of documents indexed to the data stream",
+		[]string{"data_stream"},
+		nil,
+	)
+	dataStreamStatsIndexingIndexTimeSecondsTotal = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "data_stream_stats", "indexing_index_time_seconds_total"),
+		"Total time in seconds spent indexing documents to the data stream",
+		[]string{"data_stream"},
+		nil,
+	)
+	dataStreamStatsIndexingIndexCurrent = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "data_stream_stats", "indexing_index_current"),
+		"Number of documents currently being indexed to the data stream",
+		[]string{"data_stream"},
+		nil,
+	)
+	dataStreamStatsIndexingDeleteTotal = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "data_stream_stats", "indexing_delete_total"),
+		"Total number of documents deleted from the data stream",
+		[]string{"data_stream"},
+		nil,
+	)
+	dataStreamStatsIndexingDeleteTimeSecondsTotal = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "data_stream_stats", "indexing_delete_time_seconds_total"),
+		"Total time in seconds spent deleting documents from the data stream",
+		[]string{"data_stream"},
+		nil,
+	)
+	dataStreamStatsSearchQueryTotal = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "data_stream_stats", "search_query_total"),
+		"Total number of search queries executed on the data stream",
+		[]string{"data_stream"},
+		nil,
+	)
+	dataStreamStatsSearchQueryTimeSecondsTotal = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "data_stream_stats", "search_query_time_seconds_total"),
+		"Total time in seconds spent on search queries on the data stream",
+		[]string{"data_stream"},
+		nil,
+	)
+	dataStreamStatsSearchFetchTotal = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "data_stream_stats", "search_fetch_total"),
+		"Total number of search fetch operations on the data stream",
+		[]string{"data_stream"},
+		nil,
+	)
+	dataStreamStatsSearchFetchTimeSecondsTotal = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "data_stream_stats", "search_fetch_time_seconds_total"),
+		"Total time in seconds spent on search fetch operations on the data stream",
+		[]string{"data_stream"},
+		nil,
+	)
+	dataStreamStatsDocsTotal = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "data_stream_stats", "docs_total"),
+		"Total number of documents in the data stream",
 		[]string{"data_stream"},
 		nil,
 	)
@@ -83,6 +144,20 @@ type DataStreamStatsDataStream struct {
 	MaximumTimestamp int64  `json:"maximum_timestamp"`
 }
 
+// dataStreamAggregatedStats holds aggregated index stats for a single data stream
+type dataStreamAggregatedStats struct {
+	indexingIndexTotal        int64
+	indexingIndexTimeMillis   int64
+	indexingIndexCurrent      int64
+	indexingDeleteTotal       int64
+	indexingDeleteTimeMillis  int64
+	searchQueryTotal          int64
+	searchQueryTimeMillis     int64
+	searchFetchTotal          int64
+	searchFetchTimeMillis     int64
+	docsCount                 int64
+}
+
 func (ds *DataStream) Update(ctx context.Context, ch chan<- prometheus.Metric) error {
 	var dsr DataStreamStatsResponse
 
@@ -98,8 +173,6 @@ func (ds *DataStream) Update(ctx context.Context, ch chan<- prometheus.Metric) e
 	}
 
 	for _, dataStream := range dsr.DataStreamStats {
-		fmt.Printf("Metric: %+v", dataStream)
-
 		ch <- prometheus.MustNewConstMetric(
 			dataStreamBackingIndicesTotal,
 			prometheus.CounterValue,
@@ -115,5 +188,135 @@ func (ds *DataStream) Update(ctx context.Context, ch chan<- prometheus.Metric) e
 		)
 	}
 
+	if len(dsr.DataStreamStats) == 0 {
+		return nil
+	}
+
+	// Build comma-separated list of data stream names for index stats query
+	dsNames := make([]string, 0, len(dsr.DataStreamStats))
+	for _, d := range dsr.DataStreamStats {
+		dsNames = append(dsNames, d.DataStream)
+	}
+
+	indexStatsURL := ds.u.ResolveReference(&url.URL{Path: "/" + strings.Join(dsNames, ",") + "/_stats"})
+	q := indexStatsURL.Query()
+	q.Set("ignore_unavailable", "true")
+	indexStatsURL.RawQuery = q.Encode()
+
+	indexResp, err := getURL(ctx, ds.hc, ds.logger, indexStatsURL.String())
+	if err != nil {
+		ds.logger.Warn("failed to fetch index stats for data streams", "err", err)
+		return nil
+	}
+
+	var isr indexStatsResponse
+	if err := json.Unmarshal(indexResp, &isr); err != nil {
+		ds.logger.Warn("failed to unmarshal index stats for data streams", "err", err)
+		return nil
+	}
+
+	// Aggregate index stats per data stream by matching backing index names
+	aggregated := make(map[string]*dataStreamAggregatedStats)
+	for _, dsName := range dsNames {
+		aggregated[dsName] = &dataStreamAggregatedStats{}
+	}
+
+	for indexName, indexStats := range isr.Indices {
+		dsName := resolveDataStreamName(indexName, dsNames)
+		if dsName == "" {
+			continue
+		}
+
+		agg := aggregated[dsName]
+		p := indexStats.Primaries
+
+		agg.indexingIndexTotal += p.Indexing.IndexTotal
+		agg.indexingIndexTimeMillis += p.Indexing.IndexTimeInMillis
+		agg.indexingIndexCurrent += p.Indexing.IndexCurrent
+		agg.indexingDeleteTotal += p.Indexing.DeleteTotal
+		agg.indexingDeleteTimeMillis += p.Indexing.DeleteTimeInMillis
+		agg.searchQueryTotal += p.Search.QueryTotal
+		agg.searchQueryTimeMillis += p.Search.QueryTimeInMillis
+		agg.searchFetchTotal += p.Search.FetchTotal
+		agg.searchFetchTimeMillis += p.Search.FetchTimeInMillis
+		agg.docsCount += p.Docs.Count
+	}
+
+	for _, dsName := range dsNames {
+		agg := aggregated[dsName]
+
+		ch <- prometheus.MustNewConstMetric(
+			dataStreamStatsIndexingIndexTotal,
+			prometheus.CounterValue,
+			float64(agg.indexingIndexTotal),
+			dsName,
+		)
+		ch <- prometheus.MustNewConstMetric(
+			dataStreamStatsIndexingIndexTimeSecondsTotal,
+			prometheus.CounterValue,
+			float64(agg.indexingIndexTimeMillis)/1000.0,
+			dsName,
+		)
+		ch <- prometheus.MustNewConstMetric(
+			dataStreamStatsIndexingIndexCurrent,
+			prometheus.GaugeValue,
+			float64(agg.indexingIndexCurrent),
+			dsName,
+		)
+		ch <- prometheus.MustNewConstMetric(
+			dataStreamStatsIndexingDeleteTotal,
+			prometheus.CounterValue,
+			float64(agg.indexingDeleteTotal),
+			dsName,
+		)
+		ch <- prometheus.MustNewConstMetric(
+			dataStreamStatsIndexingDeleteTimeSecondsTotal,
+			prometheus.CounterValue,
+			float64(agg.indexingDeleteTimeMillis)/1000.0,
+			dsName,
+		)
+		ch <- prometheus.MustNewConstMetric(
+			dataStreamStatsSearchQueryTotal,
+			prometheus.CounterValue,
+			float64(agg.searchQueryTotal),
+			dsName,
+		)
+		ch <- prometheus.MustNewConstMetric(
+			dataStreamStatsSearchQueryTimeSecondsTotal,
+			prometheus.CounterValue,
+			float64(agg.searchQueryTimeMillis)/1000.0,
+			dsName,
+		)
+		ch <- prometheus.MustNewConstMetric(
+			dataStreamStatsSearchFetchTotal,
+			prometheus.CounterValue,
+			float64(agg.searchFetchTotal),
+			dsName,
+		)
+		ch <- prometheus.MustNewConstMetric(
+			dataStreamStatsSearchFetchTimeSecondsTotal,
+			prometheus.CounterValue,
+			float64(agg.searchFetchTimeMillis)/1000.0,
+			dsName,
+		)
+		ch <- prometheus.MustNewConstMetric(
+			dataStreamStatsDocsTotal,
+			prometheus.GaugeValue,
+			float64(agg.docsCount),
+			dsName,
+		)
+	}
+
 	return nil
+}
+
+// resolveDataStreamName matches a backing index name to its data stream.
+// Backing indices follow the pattern .ds-{data_stream_name}-{date}-{generation}.
+func resolveDataStreamName(indexName string, dsNames []string) string {
+	for _, dsName := range dsNames {
+		if strings.HasPrefix(indexName, ".ds-"+dsName+"-") {
+			return dsName
+		}
+	}
+	return ""
 }
--- collector/data_stream_test.go
+++ collector/data_stream_test.go
@@ -28,13 +28,15 @@ import (
 
 func TestDataStream(t *testing.T) {
 	tests := []struct {
-		name string
-		file string
-		want string
+		name           string
+		dsStatsFile    string
+		indexStatsFile string
+		want           string
 	}{
 		{
-			name: "7.15.0",
-			file: "../fixtures/datastream/7.15.0.json",
+			name:           "7.15.0",
+			dsStatsFile:    "../fixtures/datastream/7.15.0.json",
+			indexStatsFile: "../fixtures/datastream/7.15.0-index-stats.json",
 			want: `# HELP elasticsearch_data_stream_backing_indices_total Number of backing indices
             # TYPE elasticsearch_data_stream_backing_indices_total counter
             elasticsearch_data_stream_backing_indices_total{data_stream="bar"} 2
@@ -43,19 +45,72 @@ func TestDataStream(t *testing.T) {
             # TYPE elasticsearch_data_stream_store_size_bytes counter
             elasticsearch_data_stream_store_size_bytes{data_stream="bar"} 6.7382272e+08
             elasticsearch_data_stream_store_size_bytes{data_stream="foo"} 4.29205396e+08
+            # HELP elasticsearch_data_stream_stats_docs_total Total number of documents in the data stream
+            # TYPE elasticsearch_data_stream_stats_docs_total gauge
+            elasticsearch_data_stream_stats_docs_total{data_stream="bar"} 1300
+            elasticsearch_data_stream_stats_docs_total{data_stream="foo"} 1000
+            # HELP elasticsearch_data_stream_stats_indexing_delete_time_seconds_total Total time in seconds spent deleting documents from the data stream
+            # TYPE elasticsearch_data_stream_stats_indexing_delete_time_seconds_total counter
+            elasticsearch_data_stream_stats_indexing_delete_time_seconds_total{data_stream="bar"} 0.5
+            elasticsearch_data_stream_stats_indexing_delete_time_seconds_total{data_stream="foo"} 0.25
+            # HELP elasticsearch_data_stream_stats_indexing_delete_total Total number of documents deleted from the data stream
+            # TYPE elasticsearch_data_stream_stats_indexing_delete_total counter
+            elasticsearch_data_stream_stats_indexing_delete_total{data_stream="bar"} 100
+            elasticsearch_data_stream_stats_indexing_delete_total{data_stream="foo"} 50
+            # HELP elasticsearch_data_stream_stats_indexing_index_current Number of documents currently being indexed to the data stream
+            # TYPE elasticsearch_data_stream_stats_indexing_index_current gauge
+            elasticsearch_data_stream_stats_indexing_index_current{data_stream="bar"} 3
+            elasticsearch_data_stream_stats_indexing_index_current{data_stream="foo"} 3
+            # HELP elasticsearch_data_stream_stats_indexing_index_time_seconds_total Total time in seconds spent indexing documents to the data stream
+            # TYPE elasticsearch_data_stream_stats_indexing_index_time_seconds_total counter
+            elasticsearch_data_stream_stats_indexing_index_time_seconds_total{data_stream="bar"} 6.5
+            elasticsearch_data_stream_stats_indexing_index_time_seconds_total{data_stream="foo"} 5
+            # HELP elasticsearch_data_stream_stats_indexing_index_total Total number of documents indexed to the data stream
+            # TYPE elasticsearch_data_stream_stats_indexing_index_total counter
+            elasticsearch_data_stream_stats_indexing_index_total{data_stream="bar"} 1300
+            elasticsearch_data_stream_stats_indexing_index_total{data_stream="foo"} 1000
+            # HELP elasticsearch_data_stream_stats_search_fetch_time_seconds_total Total time in seconds spent on search fetch operations on the data stream
+            # TYPE elasticsearch_data_stream_stats_search_fetch_time_seconds_total counter
+            elasticsearch_data_stream_stats_search_fetch_time_seconds_total{data_stream="bar"} 1.6
+            elasticsearch_data_stream_stats_search_fetch_time_seconds_total{data_stream="foo"} 1.2
+            # HELP elasticsearch_data_stream_stats_search_fetch_total Total number of search fetch operations on the data stream
+            # TYPE elasticsearch_data_stream_stats_search_fetch_total counter
+            elasticsearch_data_stream_stats_search_fetch_total{data_stream="bar"} 800
+            elasticsearch_data_stream_stats_search_fetch_total{data_stream="foo"} 600
+            # HELP elasticsearch_data_stream_stats_search_query_time_seconds_total Total time in seconds spent on search queries on the data stream
+            # TYPE elasticsearch_data_stream_stats_search_query_time_seconds_total counter
+            elasticsearch_data_stream_stats_search_query_time_seconds_total{data_stream="bar"} 5
+            elasticsearch_data_stream_stats_search_query_time_seconds_total{data_stream="foo"} 5
+            # HELP elasticsearch_data_stream_stats_search_query_total Total number of search queries executed on the data stream
+            # TYPE elasticsearch_data_stream_stats_search_query_total counter
+            elasticsearch_data_stream_stats_search_query_total{data_stream="bar"} 1000
+            elasticsearch_data_stream_stats_search_query_total{data_stream="foo"} 1000
 			`,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			f, err := os.Open(tt.file)
+			fDsStats, err := os.Open(tt.dsStatsFile)
 			if err != nil {
 				t.Fatal(err)
 			}
-			defer f.Close()
+			defer fDsStats.Close()
+
+			fIndexStats, err := os.Open(tt.indexStatsFile)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer fIndexStats.Close()
 
 			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				io.Copy(w, f)
+				switch {
+				case r.URL.Path == "/_data_stream/*/_stats":
+					io.Copy(w, fDsStats)
+				case strings.HasSuffix(r.URL.Path, "/_stats"):
+					io.Copy(w, fIndexStats)
+				default:
+					http.Error(w, "Not Found", http.StatusNotFound)
+				}
 			}))
 			defer ts.Close()
 
